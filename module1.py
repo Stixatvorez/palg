50d61e87734cc9d46f9102739bfc52eb39674276
mark salumets <93312154+Stixatvorez@users.noreply.github.com>
2021-12-07 12:52:09 +0200

    Update module1.py

--- module1.py
+++ module1.py
@@ -1,34 +1,32 @@
 ﻿def adding():
-    #Программа добавляет в списки имя и зарплату
-    #esimene arv nimi: str
-    #teine arv - palk: int 
-    #rtype var: str
+ """Добавление человека и зп
+ """
 	nimi=input("Введите имя: ")
 	palk=input("Введите зарплату: ")
-	with open("inimesed.txt", "a") as inimesed:
+	with open("inimesed.txt", "a") as inimesed:#добовляем человека
 		inimesed.write(nimi+"\n")	
-	with open("palgad.txt", "a") as palgad:
+	with open("palgad.txt", "a") as palgad:#добовляем зп
 		palgad.write(palk+"\n")
 def maks():
-     #проверяет списки и выводит на экран человека с максимальной зарплатой
-    #rtype var: int
+"""вычисление самой большой зп
+"""
 	palgad=[]
-	with open("palgad.txt", "r") as f1:
+	with open("palgad.txt", "r") as f1:#открываем файл зп
 		for stro in f1:
 			palgad.append(stro.strip())
 	f=open("inimesed.txt", "r")
 	inimesed=[]
 	for stroka in f:
 		inimesed.append(stroka.strip())
-	f.close
+	f.close#закрываем файл
 	zp=palgad.copy()
 	zp.sort()
 	a=zp[0]
 	b=palgad.index(a)
 	print("Самая большая зарплата у "+inimesed[b])
 def min():
-    #проверяет списки и выводит на экран человека с самой маленькой зарплатой
-    #rtype var: int
+"""вычисление мин зп
+"""
 	palgad=[]
 	with open("palgad.txt", "r") as f1:
 		for strok in f1:
@@ -45,9 +43,7 @@ def min():
 	b=palgad.index(a)
 	print("Самая маленькая зарплата у "+inimesed[b])
 def keskmine():
-    """
-    проверяет списки и выводит на экран среднюю зарплату
-    rtype var:int
+    """проверяет списки и выводит на экран среднюю зарплату
     """
     sum=0
     for palgad in p:
